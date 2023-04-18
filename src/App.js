cd9c2cbd0528fa9fc449b4a2dddd398f85eb1960
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-18 22:13:53 +0200

    renders list and make activities state persistent in local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Form from "./components/Form";
+import ActivityList from "./components/ActivityList";
 import { useState } from "react";
 import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  // const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
 
   // rendering activities
   function handleAddActivity(newActivity) {
@@ -15,6 +20,7 @@ function App() {
     <div className="App">
       <header className="App-header">Weather APP</header>
       <Form onAddActivity={handleAddActivity} />
+      <ActivityList activities={activities}></ActivityList>
     </div>
   );
 }
