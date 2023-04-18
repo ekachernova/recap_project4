3461aeed3ca989ecfedcdcdded1404c58f0f4e26
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-18 20:22:07 +0200

    unique id for every activity object works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-
-import './App.css';
-import Form from './components/Form';
-import { useState } from 'react';
-import { uid } from 'uid';
+import "./App.css";
+import Form from "./components/Form";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
-  const [activities,setActivities] = useState([]);
- 
- // rendering activities
- function handleAddActivity(newActivity) {
-  setActivities([{id:uid(),...newActivity},...activities]);
+  const [activities, setActivities] = useState([]);
 
-
- }
+  // rendering activities
+  function handleAddActivity(newActivity) {
+    setActivities([{ id: uid(), ...newActivity }, ...activities]);
+    console.log("activities:", activities);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        Weather APP
-      </header>
-      <Form onAddActivity={handleAddActivity}/>
-    
+      <header className="App-header">Weather APP</header>
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,42 +1,36 @@
+export default function Form({ onAddActivity }) {
+  // get the data from the form
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newActivity = Object.fromEntries(formData);
 
-export default function Form({onAddActivity}) {
-    
-    
-    // get the data from the form
-    function handleSubmit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const newActivity = Object.fromEntries(formData);
-    
-        if (event.target.elements.isForGoodWeather.checked) {
-            console.log("is checked");
-        }
-        // onAddActivity(data);
-        console.log(newActivity);
-        console.log(event.target.isForGoodWeather.checked);
-        event.target.reset();
-        event.target.elements.name.focus();
-        
-       
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-        <h2>Add new Activity</h2>
+    // if (event.target.elements.isForGoodWeather.checked) {
+    //   console.log("is checked");
+    // }
+    onAddActivity(newActivity);
+    // console.log(newActivity);
+    // console.log(event.target.isForGoodWeather.checked);
+    event.target.reset();
+    event.target.elements.name.focus();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Add new Activity</h2>
 
-        <div className = "form__input">
-        <label htmlFor = "name">Name:</label>
-        <input name="name" type ="text"></input>
-        </div>
+      <div className="form__input">
+        <label htmlFor="name">Name:</label>
+        <input name="name" type="text"></input>
+      </div>
 
-        <div className = "form__checkbox">
-        <label htmlFor = "isForGoodWeather">Good-weather activity:</label>
+      <div className="form__checkbox">
+        <label htmlFor="isForGoodWeather">Good-weather activity:</label>
         <input name="isForGoodWeather" type="checkbox"></input>
-        </div>
-        
-        <div className="form__button-wrapper">
-        <button type="submit">Submit</button>
-        </div>
-      </form>
+      </div>
 
-    )
+      <div className="form__button-wrapper">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
 }
