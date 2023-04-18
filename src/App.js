71978eed520eb1b5364a12afb0092d71fd81255d
Ekaterina Chernova <zvirbulyonok@gmail.com>
2023-04-18 17:32:58 +0200

    finish add activity form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 
 import './App.css';
 import Form from './components/Form';
+import { useState } from 'react';
+import { uid } from 'uid';
 
 function App() {
+  const [activities,setActivities] = useState([]);
+ 
+ // rendering activities
+ function handleAddActivity(newActivity) {
+  setActivities([{id:uid(),...newActivity},...activities]);
+
+
+ }
   return (
     <div className="App">
       <header className="App-header">
         Weather APP
       </header>
-      {/* <Form onAddActivity={}/> */}
-      <Form/>
-
+      <Form onAddActivity={handleAddActivity}/>
+    
     </div>
   );
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,21 +1,18 @@
 
 export default function Form({onAddActivity}) {
-    // rendering activities
-    function handleAddActivity() {
-
-    }
+    
     
     // get the data from the form
     function handleSubmit(event) {
         event.preventDefault();
         const formData = new FormData(event.target);
-        const data = Object.fromEntries(formData);
+        const newActivity = Object.fromEntries(formData);
     
         if (event.target.elements.isForGoodWeather.checked) {
             console.log("is checked");
         }
         // onAddActivity(data);
-        console.log(data);
+        console.log(newActivity);
         console.log(event.target.isForGoodWeather.checked);
         event.target.reset();
         event.target.elements.name.focus();
@@ -27,12 +24,12 @@ export default function Form({onAddActivity}) {
         <h2>Add new Activity</h2>
 
         <div className = "form__input">
-        <label forHTML = "name">Name:</label>
+        <label htmlFor = "name">Name:</label>
         <input name="name" type ="text"></input>
         </div>
 
         <div className = "form__checkbox">
-        <label forHTML = "isForGoodWeather">Good-weather activity:</label>
+        <label htmlFor = "isForGoodWeather">Good-weather activity:</label>
         <input name="isForGoodWeather" type="checkbox"></input>
         </div>
         
