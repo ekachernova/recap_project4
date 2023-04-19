bff4f5071847a231e5dd73de81fd05c10001a5e4
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-19 16:30:07 +0200

    add footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,5 @@
-      export default function Footer(){
-        return (
-            <footer className="App-footer">Weather APP 2023</footer>
-        )
-      }
-      
-    
+import "../Footer/Footer.css";
+
+export default function Footer() {
+  return <footer className="App-footer">Weather APP 2023</footer>;
+}
