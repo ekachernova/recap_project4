c45471a307834c2b72874a69c09344b0f7c62d4a
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-06-12 15:35:11 +0200

    changed README

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,5 @@
-import "../Header/Header.css"
+import "../Header/Header.css";
 
 export default function Header() {
-    return (
-        <header className="App-header">Weather APP</header>
-    )
-  }
-  
+  return <header className="App-header">Weather APP</header>;
+}
